6bc149dd31d315da37e705da5b1fdf428aee3b38
chowking <296015903@qq.com>
2022-07-20 02:04:16 +0800

    修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'/dist/',
+  publicPath:'./',
   devServer: {
     port: 8080, // 端口
     host: 'localhost',
